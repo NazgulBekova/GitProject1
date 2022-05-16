3e9d4f7fd3c0d8db5c2373160b6c81f9c3b7bd93
NazgulBekova <dairovna.n@gmail.com>
2022-05-16 08:47:48 -0400

    the udated commit

--- src/com/syntax/Main.java
+++ src/com/syntax/Main.java
@@ -4,12 +4,14 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+    // teamlead project
         System.out.println("I am Groot!!!");
         System.out.println("I am Groot2!!!");
         System.out.println("I am Groot3!!!");
 
         System.out.println("I am the correct code");
         System.out.println("I am not confused");
+        System.out.println("hello my friend");
 
     }
 }
